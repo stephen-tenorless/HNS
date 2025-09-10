9df62df1982d17245c377d912ec3b27f339f23ef
stephen-tenorless <stephen@guruward.com>
2025-09-10 09:00:43 -0700

    working nx monorepo

---
+++ apps/registration/src/polyfills.ts
@@ -0,0 +1,4 @@
+/***************************************************************************************************
+ * Zone JS is required by Angular for running change detection.
+ */
+import 'zone.js'; // Included with Angular CLI.
